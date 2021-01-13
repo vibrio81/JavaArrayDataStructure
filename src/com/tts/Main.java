212f4af1e6bb5bb5b470d177da7ba6d0485ab317
Lila Stone-Porter <74431023+vibrio81@users.noreply.github.com>
2021-01-13 16:35:55 -0600

    finished
    working product but git connectivity with intelliJ still off

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -14,6 +14,28 @@ public class Main {
         }
         System.out.println("The sum of the numbers is:" + sum);
 
+        //What is the output
+        double[ ] exampleArray = {1,5,6,5,4,1};
+
+        double maximum = exampleArray[0];
+
+        int index = 0;
+
+        for (int i = 1; i<exampleArray.length; i++){
+
+            if (exampleArray[ i ] > maximum) {
+
+                maximum = exampleArray[ i ];
+
+                index = i;
+
+            }
+
+        }
+
+        System.out.println(index);
+
+
         //doubled numbers
         int size = 4;
         int power = 2;
